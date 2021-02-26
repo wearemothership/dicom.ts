0785a3f369bf30e8e5fdacf2aa86382862549adf
Nick Hingston <nick@wearemothership.com>
2021-02-26 17:51:23 +0000

    fix jpeg 8bit baseline images

--- src/parser/constants.js
+++ src/parser/constants.js
@@ -5,7 +5,7 @@ export const TransferSyntax = {
 	compressionJpeg: "1.2.840.10008.1.2.4",
 	compressionJpegLossless: "1.2.840.10008.1.2.4.57",
 	compressionJpegLosslessSel1: "1.2.840.10008.1.2.4.70",
-	compressionJpegBaseline_8bit: "1.2.840.10008.1.2.4.50",
+	compressionJpegBaseline8bit: "1.2.840.10008.1.2.4.50",
 	compressionJpegBaseline12bit: "1.2.840.10008.1.2.4.51",
 	compressionJpegLsLossless: "1.2.840.10008.1.2.4.80",
 	compressionJpegLs: "1.2.840.10008.1.2.4.81",
