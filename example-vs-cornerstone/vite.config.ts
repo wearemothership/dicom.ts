d02aa31308175fe9a58ab642cb30a5fe40458a7e
Nick Hingston <nick@wearemothership.com>
2025-03-12 14:36:11 +0000

    fix ghpages deploy

--- example-vs-cornerstone/vite.config.ts
+++ example-vs-cornerstone/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig, PluginOption } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: './',
   plugins: [react() as PluginOption], // not sure why the cast is needed
   server: {
     host: true,
